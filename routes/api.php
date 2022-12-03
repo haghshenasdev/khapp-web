959d2ca79567a79056f627a7a88b01a038fb465a
Mohammad Mahdi Haghshenas <mhgorgab@gmail.com>
2022-12-03 22:17:55 +0330

    start adding multi charity

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/{charity}',function (\App\Models\charity $charity){
+    return $charity;
+    //write home data geter
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
