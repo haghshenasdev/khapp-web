b5912af29aa349268b3f0dc4542e9c3b5cf59862
mohammad mahdi <mhgorgab@gmail.com>
2022-12-21 10:23:31 +0330

    update verify pay

--- app/Http/Controllers/api/v1/Pay.php
+++ app/Http/Controllers/api/v1/Pay.php
@@ -49,7 +49,7 @@ class Pay extends Controller
 
         try {
             // Get amount & transaction_id from database or gateway request
-            $fk = Faktoor::query()->find($request->faktoorId)->first();
+            $fk = Faktoor::query()->find($request->faktoorId);
             if (isNull($fk)){
                 return response()->json([
                     'message' => 'فاکتور یافت نشد.',
