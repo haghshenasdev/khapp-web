ff48d82c088fb755d470031e3a64a87d5c5e890a
mohammad mahdi <mhgorgab@gmail.com>
2022-12-21 10:30:07 +0330

    tst

--- app/Http/Controllers/api/v1/Pay.php
+++ app/Http/Controllers/api/v1/Pay.php
@@ -56,6 +56,7 @@ class Pay extends Controller
 //                    'status' => 'error'
 //                ]);
 //            }
+            dd($fk);
             $invoice = new Invoice($fk->amount,$fk->ResNum);
             $receipt = PaymentGateway::verify($invoice);
             // Save receipt data and return response
