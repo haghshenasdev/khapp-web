916ecd740c2cbab5aefeb72447676aad9252370c
mohammad mahdi <mhgorgab@gmail.com>
2023-01-08 11:44:47 +0330

    bugfix in route users

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::group(['prefix' => 'dashboard','middleware' => ['auth']],function (){
     Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
     Route::get('profile', [App\Http\Controllers\Profile::class, 'index'])->name('profile');
 
-    Route::get('users', [App\Http\Controllers\users::class, 'index'])->name('users')->middleware('can:see-users');
+    Route::get('users', [App\Http\Controllers\Users::class, 'index'])->name('users')->middleware('can:see-users');
     Route::get('darkhasts', [App\Http\Controllers\Darkhasts::class, 'index'])->name('darkhasts');
     Route::get('charities', [App\Http\Controllers\Charities::class, 'index'])->name('charities')->middleware('can:see-charities');
     Route::get('charities/new-charity', [App\Http\Controllers\Charities::class, 'new'])->name('newCharity')->middleware('can:see-charities');
