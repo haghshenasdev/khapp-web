28130132be5dd8271de766624751e631c1985cdc
mohammad mahdi <mhgorgab@gmail.com>
2023-01-12 09:44:53 +0330

    chenge config

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'https://khapp.amiralmomenin-kheirieh.ir/verify'
+        'https://khapp.kheiriehemamali.ir/verify'
     ];
 }
