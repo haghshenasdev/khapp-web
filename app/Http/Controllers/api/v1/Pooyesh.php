dfd7522212549cef6544ca1e73420ab64fd4d8f6
Mohammad Mahdi Haghshenas <mhgorgab@gmail.com>
2023-03-13 09:41:58 +0330

    bug fix in persent tamin in pooyesh api

--- app/Http/Controllers/api/v1/Pooyesh.php
+++ app/Http/Controllers/api/v1/Pooyesh.php
@@ -16,7 +16,7 @@ class Pooyesh extends Controller
 
         if ($request->has('id')){
             $data = $queryBase->findOrFail($request->input('id'))->toArray();
-            if (!is_null($data['amount'])){
+            if (!is_null($data['amount']) and $data['amount'] != 0){
                 $data['persent_tamin']  = Faktoor::query()->where('is_pardakht',1)->where('type',$data['type_pay'])->where('charity',$charity)->sum('amount') * 100 / $data['amount'];
             }
             return [
