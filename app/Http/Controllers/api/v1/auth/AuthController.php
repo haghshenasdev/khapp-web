67a482dd72e9c8e373d6c98a277728278e10ffc0
haghshenas <mhgorgab@gmail.com>
2023-10-10 00:43:49 +0330

    nullabeled email in register api

--- app/Http/Controllers/api/v1/auth/AuthController.php
+++ app/Http/Controllers/api/v1/auth/AuthController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers\api\v1\auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Validator;
+use Laravel\Socialite\Facades\Socialite;
+use Laravel\Socialite\Contracts\User as SocialiteUser;
 
-class AuthController extends baseApiAuth
+class
+AuthController extends baseApiAuth
 {
-    public function login(Request $request)
+    public function login(Request $request): \Illuminate\Http\JsonResponse
     {
         $validator =  validator($request->all(),[
             'phone' => ['required','regex:/(09)[0-9]{9}/','digits:11','numeric'],
@@ -52,7 +56,7 @@ class AuthController extends baseApiAuth
         $validator =  validator($request->all(),[
             'name' => ['required', 'string', 'max:255'],
             'phone' => ['required','regex:/(09)[0-9]{9}/','digits:11','numeric','unique:'.User::class],
-            'email' => ['string', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['nullable','string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed'],
             'device_name' => 'required',
         ]);
@@ -77,6 +81,46 @@ class AuthController extends baseApiAuth
                 'token' => $token,
             ],
             'status' => 'success'
+        ]);
+    }
+
+    public function getUrlRedirectToAuth(): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'url' => Socialite::driver('google')
+                ->stateless()
+                ->redirect()
+                ->getTargetUrl(),
+        ]);
+    }
+
+    public function handleGoogleAuthCallback()
+    {
+        try {
+            /** @var SocialiteUser $socialiteUser */
+            $socialiteUser = Socialite::driver('google')->stateless()->user();
+        } catch (ClientException $e) {
+            return response()->json(['error' => 'Invalid credentials provided.'], 422);
+        }
+
+        dd($socialiteUser,$socialiteUser->getName());
+
+        /** @var User $user */
+        $user = User::query()
+            ->firstOrCreate(
+                [
+                    'email' => $socialiteUser->getEmail(),
+                ],
+                [
+                    'email_verified_at' => now(),
+                    'name' => $socialiteUser->getName(),
+                ]
+            );
+
+        return response()->json([
+            'user' => $user,
+            'access_token' => $user->createToken('google-token')->plainTextToken,
+            'token_type' => 'Bearer',
         ]);
     }
 }
